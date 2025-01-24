358b6db671de84659cd90d5c3f5b2e696ff392d0
Pablo Perez Arcos <pabloperezarcos@gmail.com>
2025-01-24 11:58:53 -0300

    Update dashboard.component.ts

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -11,23 +11,27 @@ import {
   CategoryScale,
   Tooltip,
 } from 'chart.js';
+import { RouterModule } from '@angular/router';
+import { VehiculosService } from '../../services/vehiculos.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  totalVehiculos: number = 0;
   public chartData: any;
   public chartOptions: any;
 
-  constructor() {
+  constructor(private vehiculosService: VehiculosService) {
     Chart.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale, Tooltip);
   }
 
   ngOnInit(): void {
     // Configuración de datos para el gráfico
+    this.cargarTotalVehiculos();
     this.chartData = {
       labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
       datasets: [
@@ -38,7 +42,7 @@ export class DashboardComponent implements OnInit {
           backgroundColor: 'rgba(255, 87, 51, 0.2)',
           fill: true,
         },
-      ],
+      ]
     };
 
     // Configuración de opciones para el gráfico
@@ -63,4 +67,25 @@ export class DashboardComponent implements OnInit {
       },
     };
   }
+
+  cargarTotalVehiculos(): void {
+    this.vehiculosService.getTotalVehiculos().subscribe({
+      next: (response) => {
+        if (response.success) {
+          this.totalVehiculos = response.total;
+        } else {
+          console.error('Error al obtener el total de vehículos:', response.message);
+        }
+      },
+      error: (error) => {
+        console.error('Error en la solicitud:', error);
+      },
+    });
+  }
+
+
+
+
+
+
 }
