5621825ca162e76e343539b63d99e02d35a5da38
Pablo Perez Arcos <pabloperezarcos@gmail.com>
2025-01-24 10:31:51 -0300

    Update vehiculos.component.ts

--- src/app/components/vehiculos/vehiculos.component.ts
+++ src/app/components/vehiculos/vehiculos.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { VehiculosService } from '../../services/vehiculos.service';
 
 @Component({
   selector: 'app-vehiculos',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './vehiculos.component.html',
   styleUrl: './vehiculos.component.scss'
 })
@@ -20,7 +21,12 @@ export class VehiculosComponent implements OnInit {
   cargarVehiculos(): void {
     this.vehiculosService.getVehiculos().subscribe({
       next: (data) => {
-        this.vehiculos = data;
+        console.log('Datos recibidos:', data); // Verifica el contenido de los datos
+        if (data && Array.isArray(data.data)) {
+          this.vehiculos = data.data; // Asignar solo si es un array válido
+        } else {
+          this.vehiculos = []; // Asegurar que sea un array, incluso si no se reciben datos válidos
+        }
       },
       error: (error) => {
         console.error('Error al obtener los vehículos:', error);
@@ -29,6 +35,6 @@ export class VehiculosComponent implements OnInit {
         console.log('Carga de vehículos completada');
       },
     });
-
   }
+
 }
