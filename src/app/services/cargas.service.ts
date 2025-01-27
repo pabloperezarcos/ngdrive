9156d00b37260218038c38d302785e37b0fd28e6
Pablo Perez Arcos <pabloperezarcos@gmail.com>
2025-01-27 11:30:45 -0300

    Create cargas.service.ts

---
+++ src/app/services/cargas.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CargasService {
+  private apiUrl = 'https://www.carnesag.cl/ngdrive/api/obtenerCargas.php';
+
+  constructor(private http: HttpClient) { }
+
+  obtenerCargas(): Observable<any[]> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http.get<any>(this.apiUrl, { headers });
+  }
+
+}
