0e0ebf499703544f44e22c248dfecb1f7b1046cd
Pablo Perez Arcos <pabloperezarcos@gmail.com>
2025-01-27 11:47:59 -0300

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { LOCALE_ID } from '@angular/core';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-  provideHttpClient(withFetch()),
-  provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withFetch()),
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'es-CL' }
+  ]
 };
