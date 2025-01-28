3f52e169ef5bef95fadc97f872e0f1f3f00d76df
Pablo Perez Arcos <pabloperezarcos@gmail.com>
2025-01-28 09:12:49 -0300

    Create component Informes

---
+++ src/app/components/informes/informes.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+
+
+@Component({
+  selector: 'app-informes',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './informes.component.html',
+  styleUrl: './informes.component.scss'
+})
+export class InformesComponent {
+
+  informeId: string | null = null;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.informeId = this.route.snapshot.paramMap.get('informeId');
+  }
+
+
+
+
+  
+}
+
